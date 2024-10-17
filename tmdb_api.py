709404176678f6688fe81e3f9d57232b2e9cdb95
NoCodeDan <24924055-NoCodeDan@users.noreply.replit.com>
2024-10-17 12:57:18 +0000

    Restored to '75eb771f5f51a667999ca43be33a7240e3a44f2c'

--- tmdb_api.py
+++ tmdb_api.py
@@ -1,6 +1,5 @@
 import os
 import requests
-from datetime import datetime
 
 TMDB_API_KEY = os.environ.get('TMDB_API_KEY')
 BASE_URL = 'https://api.themoviedb.org/3'
@@ -17,7 +16,7 @@ def get_popular_movies(page=1, genre_id=None):
     response = requests.get(url, params=params)
     return response.json()
 
-def get_popular_tv_shows(page=1, genre_id=None, region='US', year=None):
+def get_popular_tv_shows(page=1, genre_id=None, region='US'):
     url = f'{BASE_URL}/discover/tv'
     params = {
         'api_key': TMDB_API_KEY,
@@ -27,8 +26,6 @@ def get_popular_tv_shows(page=1, genre_id=None, region='US', year=None):
     }
     if genre_id:
         params['with_genres'] = genre_id
-    if year:
-        params['first_air_date_year'] = year
     response = requests.get(url, params=params)
     return response.json()
 
